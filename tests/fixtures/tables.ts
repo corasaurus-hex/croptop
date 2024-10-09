95b791ced49fac469cc46aa17a7c5969c6b3dad9
Cora Sutton <cora@sutton.me>
2024-10-09 02:01:31 -0500

    Add more test setup

---
+++ tests/fixtures/tables.ts
@@ -0,0 +1,70 @@
+import postgres from "npm:postgres";
+
+export const isTableExists = async (sql: postgres.Sql, tableName: string) =>
+  (await sql`SELECT to_regclass(${tableName}) IS NOT NULL AS exists`)[0].exists;
+
+export const createOrgTable = async (sql: postgres.Sql) =>
+  isTableExists(sql, "org") ||
+  (await sql`
+    CREATE TABLE IF NOT EXISTS org
+    (
+        id   BIGSERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        url  TEXT
+    )`);
+
+export const createUserTable = async (sql: postgres.Sql) =>
+  isTableExists(sql, "user") ||
+  (await sql`
+    CREATE TABLE IF NOT EXISTS "user"
+    (
+        id     BIGSERIAL PRIMARY KEY,
+        name   TEXT        NOT NULL,
+        email  TEXT UNIQUE NOT NULL,
+        org_id BIGINT REFERENCES org (id)
+    )`);
+
+export const createWidgetTable = async (sql: postgres.Sql) =>
+  isTableExists(sql, "widget") ||
+  (await sql`
+    CREATE TABLE IF NOT EXISTS widget
+    (
+        id          BIGSERIAL PRIMARY KEY,
+        name        TEXT NOT NULL,
+        description TEXT,
+        org_id      BIGINT REFERENCES org (id),
+        UNIQUE (id, org_id)
+    )`);
+
+export const createPartTable = async (sql: postgres.Sql) =>
+  isTableExists(sql, "part") ||
+  (await sql`
+      CREATE TABLE IF NOT EXISTS part
+      (
+          id          BIGSERIAL PRIMARY KEY,
+          name        TEXT   NOT NULL,
+          description TEXT,
+          widget_id   BIGINT NOT NULL,
+          org_id      BIGINT NOT NULL,
+          FOREIGN KEY (widget_id, org_id) REFERENCES widget (id, org_id)
+      )`);
+
+export const createPartComponentTable = async (sql: postgres.Sql) =>
+  isTableExists(sql, "part_component") ||
+  (await sql`
+    CREATE TABLE IF NOT EXISTS part_component
+    (
+        id          BIGSERIAL PRIMARY KEY,
+        name        TEXT   NOT NULL,
+        description TEXT,
+        org_id      BIGINT NOT NULL,
+        part_id     BIGINT NOT NULL
+    )`);
+
+export const createTables = async (sql: postgres.Sql) => {
+  await createOrgTable(sql);
+  await createUserTable(sql);
+  await createWidgetTable(sql);
+  await createPartTable(sql);
+  await createPartComponentTable(sql);
+};
--- tests/pg_test.ts
+++ tests/pg_test.ts
@@ -1,8 +1,28 @@
-import { describe, it } from "jsr:@std/testing/bdd";
+import { afterAll, beforeAll, describe, it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
 import { extractSchemas } from "../src/pg.ts";
+import postgres from "npm:postgres";
+import { createTables } from "./fixtures/tables.ts";
 
 describe("pg tests", () => {
+  let sql: postgres.Sql;
+
+  beforeAll(async () => {
+    sql = postgres({
+      host: "localhost",
+      port: 5432,
+      user: "postgres",
+      password: "postgrespassword",
+      database: "postgres",
+    });
+
+    await createTables(sql);
+  });
+
+  afterAll(async () => {
+    await sql.end();
+  });
+
   it("should extract schemas", async () => {
     expect(
       await extractSchemas({
